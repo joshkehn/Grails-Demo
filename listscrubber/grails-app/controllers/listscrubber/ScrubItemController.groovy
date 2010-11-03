c87efd1d81d5fc13847751826c2980eb97330860
Joshua Kehn <josh@emprivo.com>
2010-11-03 18:43:16 -0400

    Views and basic controllers

---
+++ listscrubber/grails-app/controllers/listscrubber/ScrubItemController.groovy
@@ -0,0 +1,11 @@
+package listscrubber
+
+class ScrubItemController {
+
+    def index = {
+    }
+    
+    def scrublist = {
+        [messages:['File upload successful.']];
+    }
+}
---
+++ listscrubber/test/unit/listscrubber/ScrubItemControllerTests.groovy
@@ -0,0 +1,17 @@
+package listscrubber
+
+import grails.test.*
+
+class ScrubItemControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
