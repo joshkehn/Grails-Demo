c8e238fbd74015b5995ee6a32650e68fdebfbae8
Joshua Kehn <josh@emprivo.com>
2010-11-04 10:36:29 -0400

    Woo, modified view & controller

--- listscrubber/grails-app/controllers/listscrubber/ScrubItemController.groovy
+++ listscrubber/grails-app/controllers/listscrubber/ScrubItemController.groovy
@@ -5,38 +5,21 @@ import com.listscrubber.*;
 class ScrubItemController {
 
     def index = {
-        redirect(action: "scrublist", params: [initview: true])
-    }
-    
-    def scrublist = {
-		
-		
-		//def fileProcessor = new FileProcessor();
-		//def scrubber = new Scrubber();
-		
-		def dirtyFile
-		def contents;
-		if(!params.initview) {
-			dirtyFile = request.getFile("dirtylist");
-			contents = dirtyFile.inputStream.text
-		}
-				
-		String fileType = params.filetype;
-		String newFileName = params.newFileName;
-		
-		//String[] processedResults = fileProcessor(dirtyFile,fileType);
-		//String[] scrubbedResults = scrubber.scrub(processedResults, fileType);
-		//fileProcessor.saveReadyFile(scrubbedResults, newFileName);
-		//
-		//ArrayList<HashMap> urls = fileProcessor.getReadyFilesList();
-		//
-		//[readyList: urls]
-		params: [initview: true];
-
-			println 'contents: ' + contents;
-			println 'file type: ' + fileType;
-			println 'new file name: ' + newFileName;
-		
-       [messages:['File upload successful:', 'contents: ' + contents, 'file type: ' + fileType, 'new file name: ' + newFileName ], initview: false];
+        def dirtyFile;
+        if(params.filetype || request.getFile("dirtylist") || params.newFileName)
+        {
+            dirtyFile = request.getFile("dirtylist")
+            
+            def contents = dirtyFile.inputStream.text
+            def fileType = params.filetype;
+            def fileName = params.newFileName;
+            
+            println "Contents: " + contents
+            println "File Type: " + fileType
+            println "File Name: " + fileName
+            
+            
+            [success:['File upload successful.']]
+        }
     }
 }
