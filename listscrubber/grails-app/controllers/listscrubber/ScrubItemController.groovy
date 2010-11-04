231c3dd0b0ea47d3bfa1b4eefae3450779af1836
Joshua Kehn <josh@emprivo.com>
2010-11-04 13:37:26 -0400

    Controller changes

--- listscrubber/grails-app/controllers/listscrubber/ScrubItemController.groovy
+++ listscrubber/grails-app/controllers/listscrubber/ScrubItemController.groovy
@@ -3,26 +3,50 @@ package listscrubber;
 class ScrubItemController {
 
     def index = {
-        if(params.filetype && params.dirtylist && params.newFileName)
+        
+        if(params.fileType && params.dirtyList && params.fileName)
         {
             def valid = true;
-            def dirtyFile = request.getFile("dirtyfile");
+            def dirtyFile = request.getFile("dirtyFile");
             def contents = dirtyFile.inputStream.text;
-            def fileType = params.filetype;
-            def fileName = params.newFileName;
+            def fileType = params.fileType;
+            def fileName = params.fileName;
             
             println "Contents: " + contents
             println "File Type: " + fileType
             println "File Name: " + fileName
-            [success:['File upload successful.']]                
+            [successes:['File upload successful.']]                
         }
-        else if(params.filetype || params.dirtylist || params.newFileName)
+        else if(params.fileType || params.dirtyList || params.fileName)
         {
             [errors:['One or more options were not filled out. Every field is required.']];
         }
-        else
-        {
-            /* Nothing */
-        }
+    }
+    
+/*    def scrubfile = {
+        FileHandler cmd ->
+            if(cmd.hasErrors())
+            {
+                redirect(action:'index', errors:['All fields are required.']);
+            }
+            else
+            {
+/*                [successes:['File uploaded successfully.']]
+                redirect(action:'index', successes:['File uploaded successfully.']);
+            }
+
+    }*/
+}
+
+class FileHandler
+{
+    String fileType;
+    String fileName;
+    String dirtyFile;
+    
+    static constraints = {
+        fileType(blank: false)
+        fileName(blank: false)
+        dirtyFile(blank: false)
     }
 }
