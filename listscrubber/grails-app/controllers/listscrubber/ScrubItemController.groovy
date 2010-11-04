155eb735d85a003acf8a068e008b0237cea40387
rvilensky <ramy.vilensky@kobemail.com>
2010-11-04 13:38:30 -0400

    fixes

--- listscrubber/grails-app/controllers/listscrubber/ScrubItemController.groovy
+++ listscrubber/grails-app/controllers/listscrubber/ScrubItemController.groovy
@@ -1,28 +1,46 @@
 package listscrubber;
 
-class ScrubItemController {
+ class ScrubItemController {
 
-    def index = {
-        if(params.filetype && params.dirtylist && params.newFileName)
-        {
-            def valid = true;
-            def dirtyFile = request.getFile("dirtyfile");
-            def contents = dirtyFile.inputStream.text;
-            def fileType = params.filetype;
-            def fileName = params.newFileName;
-            
-            println "Contents: " + contents
-            println "File Type: " + fileType
-            println "File Name: " + fileName
-            [success:['File upload successful.']]                
-        }
-        else if(params.filetype || params.dirtylist || params.newFileName)
-        {
-            [errors:['One or more options were not filled out. Every field is required.']];
-        }
-        else
-        {
-            /* Nothing */
-        }
-    }
-}
+     def index = {
+
+		if(params.filetype && params.dirtylist && params.newFileName) {
+		
+			
+			def valid = true;
+			
+			def dirtyFile = request.getFile("dirtyfile");
+			
+			def contents = dirtyFile.inputStream.text;
+			
+			def fileType = params.filetype;
+			
+			def fileName = params.newFileName;
+			
+			
+			
+			println "Contents: " + contents
+			
+			println "File Type: " + fileType
+			
+			println "File Name: " + fileName
+		
+			
+			[success:['File upload successful.']]                
+			
+		}
+			
+		else if(params.filetype || params.dirtylist || params.newFileName) {
+			
+			[errors:['One or more options were not filled out. Every field is required.']];
+			
+		}
+			
+		else {
+			
+			/* Nothing */
+			
+		}
+			
+     }
+ }
