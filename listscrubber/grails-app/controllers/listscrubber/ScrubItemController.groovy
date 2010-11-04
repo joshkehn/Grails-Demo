987e2da6cf1d9880e78c398768d495ab66fc8493
Joshua Kehn <josh@emprivo.com>
2010-11-04 11:24:34 -0400

    Revised validation

--- listscrubber/grails-app/controllers/listscrubber/ScrubItemController.groovy
+++ listscrubber/grails-app/controllers/listscrubber/ScrubItemController.groovy
@@ -1,25 +1,28 @@
-package listscrubber
-
-import com.listscrubber.*;
+package listscrubber;
 
 class ScrubItemController {
 
     def index = {
-        def dirtyFile;
-        if(params.filetype || request.getFile("dirtylist") || params.newFileName)
+        if(params.filetype && params.dirtylist && params.newFileName)
         {
-            dirtyFile = request.getFile("dirtylist")
-            
-            def contents = dirtyFile.inputStream.text
+            def valid = true;
+            def dirtyFile = request.getFile("dirtyfile");
+            def contents = dirtyFile.inputStream.text;
             def fileType = params.filetype;
             def fileName = params.newFileName;
             
             println "Contents: " + contents
             println "File Type: " + fileType
             println "File Name: " + fileName
-            
-            
-            [success:['File upload successful.']]
+            [success:['File upload successful.']]                
+        }
+        else if(params.filetype || params.dirtylist || params.newFileName)
+        {
+            [errors:['One or more options were not filled out. Every field is required.']];
+        }
+        else
+        {
+            /* Nothing */
         }
     }
 }
